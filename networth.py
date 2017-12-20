50c8216a231580007f443b350a90e0b020ea314b
codersblock <codersblock31@gmail.com>
2017-12-20 01:41:14 -0600

    formatting numbers, showing all coins in breakdown, and sorting by value

--- networth.py
+++ networth.py
@@ -15,7 +15,7 @@ f = open(os.environ["HOME"] + "/distributions.json", "r")
 distribution_hash = json.load(f);
 
 total_wealth_usd = 0.0;
-breakdown = {"ALT": 0};
+breakdown = {};
 
 for coin in distribution_hash:
     if coin == "USD":
@@ -24,15 +24,22 @@ for coin in distribution_hash:
     else:
         coin_value = (float(pull_value(coin)) * float(distribution_hash[coin]))
         total_wealth_usd += coin_value
-        if coin == "BTC":
-            breakdown["BTC"] = coin_value
-        elif coin == "ETH":
-            breakdown["ETH"] = coin_value
-        else:
-            breakdown["ALT"] += coin_value
-
+        breakdown[coin] = coin_value
 
 print ("Net Worth: $" + str(total_wealth_usd));
+
+sorted_by_percentage = [];
+percentages = {};
+
 for coin in breakdown:
     percentage = breakdown[coin] / total_wealth_usd * 100.0
-    print("  " + coin + ": " + str(breakdown[coin]) + " (" + str(percentage) + "%)")
+    percentages[coin] = percentage;
+
+    loop = 0;
+    while (loop < len(sorted_by_percentage) and percentages[sorted_by_percentage[loop]] > percentage):
+        loop += 1;
+
+    sorted_by_percentage.insert(loop, coin);
+            
+for coin in sorted_by_percentage:
+    print("  " + coin + ": $" + str(round(breakdown[coin], 2)) + " (" + str(round(percentages[coin], 2)) + "%)")
